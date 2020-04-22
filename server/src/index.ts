3028a02d7b7bcb9fdff75eaa2bc37093158061b5
NikolaNikushev <nikola.nikushev@hotmail.com>
2020-04-22 20:19:43 +0200

    Added types.

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,8 @@
 import express from "express";
+import {Request, Response} from "express";
 const app = express();
 const port = process.env.PORT || "8000";
-app.get("/", (req, res) => {
+app.get("/", (req: Request, res: Response) => {
 	res.status(200).send("WHATABYTE: Food For Devs");
 });
 
