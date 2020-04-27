de7801242bdddc9f10499a8d9ff260d7a51964d1
NikolaNikushev <nikola.nikushev@hotmail.com>
2020-04-27 10:57:45 +0200

    Added badge.

--- server/src/models/Notification.ts
+++ server/src/models/Notification.ts
@@ -6,6 +6,7 @@ export interface NotificationInput {
   title: string;
   body?: string;
   sound: string;
+  badge: number;
   data: NotificationContent;
 }
 
@@ -14,6 +15,7 @@ export class Notification implements NotificationInput {
   body?: string;
   sound: string;
   data: NotificationContent;
+  badge = 1;
   _displayInForeground = false;
 
   constructor() {
